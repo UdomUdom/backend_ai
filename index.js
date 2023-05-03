b7d5d9511df81bf4cbb2f912551e230c4242d0c2
UdomUdom <udomsak.s64@gmail.com>
2023-05-03 08:57:22 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+const app = express();
+
+//body-parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+
+//static-folder
+//app.use(express.static(path.join(__dirname, 'public'))); ---fetching
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log('Server start on port ' + port));
+
+const mysql = require('mysql2');
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log('Connected to PlanetScale!');
+
+app.post('/logins', function (req, res) {
+  const { username, password } = req.body;
+  connection.query(
+    'SELECT * FROM Users WHERE Username=' +
+      `"${username}" AND Password="${password}"`,
+    (err, result) => {
+      if (result) return res.json({ status: 200 });
+    }
+  );
+});
+
+// app.post('/signup', function (req, res) {
+//   const { username, password, email } = req.body;
+//   connection.query(
+//     'INSERT INTO Users (`username`, `password`, `email`) VALUES (?)',
+//     (err, result) => {
+//       if (result) return res.json({ status: 200 });
+//     }
+//   );
+// });
+
+app.post('/signup', (req, res) => {
+  const sql = 'INSERT INTO Users (`username`, `password`, `email`) VALUES (?)';
+  const values = [req.body.username, req.body.password, req.body.email];
+  connection.query(sql, [values], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.json({ message: 'Error in Node' });
+    } else {
+      return res.json(result);
+    }
+  });
+});
